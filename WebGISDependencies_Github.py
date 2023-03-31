0ccfce8b92f58659e136b3f7d8902f458405495c
spchawk237 <62577659+spchawk237@users.noreply.github.com>
2023-03-31 15:12:37 -0400

    Add files via upload

--- WebGISDependencies_Github.py
+++ WebGISDependencies_Github.py
@@ -32,13 +32,12 @@ ExcelOutput = r'Insert File Path Here!!!!'
 # Master Data Frame Variable
 MasterDF = pd.DataFrame()
 
-# AGOL Feature Class Variable
-AGOLFeatureClassName = []
-AGOLFeatureClassID = []
-AGOLFeatureClassURL = []
-AGOLFeatureClassOwner = []
-AGOLWebMapMatchesID = []
-AGOLWebAppMatchesID = []
+# Dictionaries
+AGOLFCName_dict = {}
+AGOLFCURL_dict = {}
+AGOLFCOwner_dict = {}
+AGOLWMID_dict = {}
+AGOLWAID_dict = {}
 
 # Find Url IDs for all feature Classes
 print('Collecting Data...')
@@ -46,45 +45,53 @@ print('Collecting Data...')
 # Grabbing all Feature Classes, Webmaps, and Web apps in Portal or AGOL
 
 FeatureClasses = gis.content.search('', item_type='Feature Service', max_items=-1)
-print(len(FeatureClasses))
 webmaps = gis.content.search('', item_type='Web Map', max_items=-1)
-print(len(webmaps))
 webapps = gis.content.search('', item_type='Application', max_items=-1)
-print(len(webapps))
 
 for fc in FeatureClasses:
     print('Starting ' + str(fc.title))
     URL = gis.content.get(fc.id).url
+    # Adding Key to Dictionaries
+    AGOLFCName_dict[fc.id] = []
+    AGOLFCURL_dict[fc.id] = []
+    AGOLFCOwner_dict[fc.id] = []
+    AGOLWMID_dict[fc.id] = []
+    AGOLWAID_dict[fc.id] = []
     # Collecting all Feature Class Names and appending them to FeatureClassName Variable
-    AGOLFeatureClassName.append(fc.title)
+    # AGOLFeatureClassName.append(fc.title)
+    AGOLFCName_dict[fc.id].append(fc.title)
     # Collecting all Feature Class IDs and appending them to FeatureClassID Variable
-    AGOLFeatureClassID.append(fc.id)
+    # AGOLFeatureClassID.append(fc.id)
     # Collecting all Feature Class URLs and appending them to FeatureClassURL Variable
-    AGOLFeatureClassURL.append(URL)
+    # AGOLFeatureClassURL.append(URL)
+    AGOLFCURL_dict[fc.id].append(URL)
     # Collecting all Feature Class Owners and appending them to FeatureClassOwner Variable
-    AGOLFeatureClassOwner.append(fc.owner)
-
-    print('Finding matches for ' + str(fc.title))
+    # AGOLFeatureClassOwner.append(fc.owner)
+    AGOLFCOwner_dict[fc.id].append(fc.owner)
     # Finding Web Map IDs that use feature class
     matches = [m.id for m in webmaps if str(m.get_data()).find(URL) > -1]
-    print('Found matches for ' + str(fc.title))
-    AGOLWebMapMatchesID.append(matches)
+
+    # If matches is not empty append to dictionary.
+    if len(matches) > 0:
+        AGOLWMID_dict.update({fc.id: matches})
+    else:
+        pass
+
     # Finding Matching Web Applications
     for w in webapps:
         try:
-            print('Finding Webapp Matches' + str(fc.title))
             # Get the JSON as a string
             wdata = str(w.get_data())
 
+            # Finding Matches
             criteria = [
                 wdata.find(URL) > -1,  # Check if URL is directly referenced
                 any([wdata.find(i) > -1 for i in matches])  # Check if any matching maps are in app
             ]
 
-            # If layer is referenced directly or indirectly, append app to list
-            if any(criteria):
-                print('Found Webapp Matches' + str(fc.title))
-                AGOLWebAppMatchesID.append(w)
+            # If there's a match append to Dictionary if not, do nothing.
+            if len(criteria) > 0:
+                AGOLWAID_dict[fc.id].append(w.id)
             else:
                 pass
 
@@ -92,14 +99,18 @@ for fc in FeatureClasses:
         except:
             continue
 
-print('Appending Data to DF then Exporting to Excel')
-# Creating Rows in FeatureClassDF for each of the lists we iterated through the for loop
-MasterDF['Feature Class Name'] = AGOLFeatureClassName
-MasterDF['Feature Class ID'] = AGOLFeatureClassID
-MasterDF['Feature Class URL'] = AGOLFeatureClassURL
-MasterDF['Feature Class Owner'] = AGOLFeatureClassOwner
-MasterDF['Web Map Matches'] = AGOLWebMapMatchesID
-MasterDF['Web App Matches'] = AGOLWebAppMatchesID
+    print('Finished ' + str(fc.title))
 
+# Using dictionaries to create Data Frames
+print('Appending Data to DF then Exporting to Excel')
+
+MasterDF['Feature Class ID'] = AGOLFCName_dict.keys()
+MasterDF['Feature Class Name'] = AGOLFCName_dict.values()
+MasterDF['Feature Class Url'] = AGOLFCURL_dict.values()
+MasterDF['Feature Class Owner'] = AGOLFCOwner_dict.values()
+MasterDF['Number of Web Maps'] = len(AGOLWMID_dict.values())
+MasterDF['Number of Web Apps'] = len(AGOLWAID_dict.values())
+
+# Exporting DF to Excel
 MasterDF.to_excel(ExcelOutput)
-print('Script Complete')
+print('Test Complete')
