85468b3562a991ab0d84900189cf0ea327b4750b
spchawk237 <62577659+spchawk237@users.noreply.github.com>
2023-04-11 10:41:47 -0400

    Add files via upload
    Bare bones finally working!

--- WebGISDependencies_Github.py
+++ WebGISDependencies_Github.py
@@ -7,7 +7,7 @@
 # Author: Andrew Parkin/GIS Manager
 #
 # Description:
-# Will show FCs and all their dependencies across your enterprise or AGOL enviroments
+# Will show FCs and all their dependencies across your enterprise or AGOL environments
 #
 #
 # Works with Enterprise GIS & ArcGIS Online
@@ -37,7 +37,11 @@ AGOLFCName_dict = {}
 AGOLFCURL_dict = {}
 AGOLFCOwner_dict = {}
 AGOLWMID_dict = {}
+AGOLWMName_dict = {}
+AGOLWMOwner_dict = {}
 AGOLWAID_dict = {}
+AGOLWAName_dict = {}
+AGOLWAOwner_dict = {}
 
 # Find Url IDs for all feature Classes
 print('Collecting Data...')
@@ -47,6 +51,11 @@ print('Collecting Data...')
 FeatureClasses = gis.content.search('', item_type='Feature Service', max_items=-1)
 webmaps = gis.content.search('', item_type='Web Map', max_items=-1)
 webapps = gis.content.search('', item_type='Application', max_items=-1)
+dashboard = gis.content.search('', item_type='Dashboard', max_items=-1)
+form = gis.content.search('', item_type='Form', max_items=-1)
+hubpage = gis.content.search('', item_type='Hub Page', max_items=-1)
+solution = gis.content.search('', item_type='Solution', max_items=-1)
+storymap = gis.content.search('', item_type='StoryMap', max_items=-1)
 
 for fc in FeatureClasses:
     print('Starting ' + str(fc.title))
@@ -56,7 +65,11 @@ for fc in FeatureClasses:
     AGOLFCURL_dict[fc.id] = []
     AGOLFCOwner_dict[fc.id] = []
     AGOLWMID_dict[fc.id] = []
+    AGOLWMName_dict[fc.id] = []
+    AGOLWMOwner_dict[fc.id] = []
     AGOLWAID_dict[fc.id] = []
+    AGOLWAName_dict[fc.id] = []
+    AGOLWAOwner_dict[fc.id] = []
     # Collecting all Feature Class Names and appending them to FeatureClassName Variable
     AGOLFCName_dict[fc.id].append(fc.title)
     # Collecting all Feature Class URLs and appending them to FeatureClassURL Variable
@@ -65,13 +78,20 @@ for fc in FeatureClasses:
     AGOLFCOwner_dict[fc.id].append(fc.owner)
     # Finding Web Map IDs that use feature class
     matches = [m.id for m in webmaps if str(m.get_data()).find(URL) > -1]
+    matchesName = [m.title for m in webmaps if str(m.get_data()).find(URL) > -1]
+    matchesOwner = [m.owner for m in webmaps if str(m.get_data()).find(URL) > -1]
 
     # If matches is not empty append to dictionary.
     if len(matches) > 0:
         AGOLWMID_dict.update({fc.id: matches})
+        AGOLWMName_dict.update({fc.id: matchesName})
+        AGOLWMOwner_dict.update({fc.id: matchesOwner})
     else:
         pass
 
+    criteria_matchesID = []
+    criteria_matchesName = []
+    criteria_matchesOwner = []
     # Finding Matching Web Applications
     for w in webapps:
         try:
@@ -84,17 +104,29 @@ for fc in FeatureClasses:
                 any([wdata.find(i) > -1 for i in matches])  # Check if any matching maps are in app
             ]
 
-            # If there's a match append to Dictionary if not, do nothing.
+            # If there's a match append if not, do nothing.
             if any(criteria):
-                AGOLWAID_dict[fc.id].append(w.id)
-        # Some apps don't have data, so we'll just skip them if they throw a TypeError
+                criteria_matchesID.append(w.id)
+                criteria_matchesName.append(w.title)
+                criteria_matchesOwner.append(w.owner)
+
+            # Some apps don't have data, so we'll just skip them if they throw a TypeError
         except:
             continue
 
+        if len(criteria_matchesID) > 0:
+            print(set(criteria_matchesID))
+            AGOLWAID_dict.update({fc.id: set(criteria_matchesID)})
+            AGOLWAName_dict.update({fc.id: set(criteria_matchesName)})
+            AGOLWAOwner_dict.update({fc.id: set(criteria_matchesOwner)})
+
     print('Finished ' + str(fc.title))
+
 
 # Using dictionaries to create Data Frames
 print('Appending Data to DF then Exporting to Excel')
+
+# Variables
 AGOLWMID_Count = []
 AGOLWAID_Count = []
 
@@ -103,16 +135,23 @@ MasterDF['Feature Class Name'] = AGOLFCName_dict.values()
 MasterDF['Feature Class Url'] = AGOLFCURL_dict.values()
 MasterDF['Feature Class Owner'] = AGOLFCOwner_dict.values()
 
+# Getting Web Map and Application Counts
 for k in AGOLWMID_dict:
     AGOLWMID_Count.append(len(AGOLWMID_dict[k]))
 
 for k in AGOLWAID_dict:
     AGOLWAID_Count.append(len(AGOLWAID_dict[k]))
 
-# Getting Web Map and Application Counts
 MasterDF['Number of Web Maps'] = AGOLWMID_Count
+MasterDF['Web Map ID'] = AGOLWMID_dict.values()
+MasterDF['Web Map Name'] = AGOLWMName_dict.values()
+MasterDF['Web Map Owner'] = AGOLWMOwner_dict.values()
 MasterDF['Number of Web Apps'] = AGOLWAID_Count
+MasterDF['Web App IDs'] = AGOLWAID_dict.values()
+MasterDF['Web App Names'] = AGOLWAName_dict.values()
+MasterDF['Web App Owner'] = AGOLWAOwner_dict.values()
 
 # Exporting DF to Excel
-MasterDF.to_excel(ExcelOutput)
-print('Test Complete')
+MasterDF.to_excel(ExcelOutput, index=False)
+
+print('Script Complete')
